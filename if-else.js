e218c8f81b32132de4fc55ebc8cc860561a663e4
komal-kumari12 <komalsahujsr34@gmail.com>
2026-05-24 09:18:43 +0530

    if-else

---
+++ if-else.js
@@ -0,0 +1,10 @@
+const age=32
+if(age<18){
+console.log("license will not approved")
+}else if(age>=18 && age<=60){
+    console.log("License will be approved")
+}else if(age>60 && age<=100){
+    console.log("License will not be approved")
+}else{
+    console.log("invalid age")
+}
